217074aac4dc7ce5d6c647f7603741ce8ff73a31
xthunder81 <xthunder81@rocketmail.com>
2023-04-29 20:25:09 +0700

    add card rotation function

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Test Script
+    void OnMouseDown() {
+        Debug.Log("BUTTON PRESSED");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
